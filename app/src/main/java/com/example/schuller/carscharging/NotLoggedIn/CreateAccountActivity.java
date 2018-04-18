01b4b36bbf7746432a453284a4d745dfbf33552a
Paul-Coman <paul_coman95@yahoo.com>
2018-04-18 20:10:32 +0300

    Sign Up and Navbar

--- app/src/main/java/com/example/schuller/carscharging/NotLoggedIn/CreateAccountActivity.java
+++ app/src/main/java/com/example/schuller/carscharging/NotLoggedIn/CreateAccountActivity.java
@@ -1,25 +1,30 @@
 package com.example.schuller.carscharging.NotLoggedIn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.schuller.carscharging.R;
 import com.example.schuller.carscharging.zModel.Driver;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class CreateAccountActivity extends AppCompatActivity {
 
-    private EditText mCreateName, mCreateEmail, mCreatePassword, mCreateCarID ;
+    private EditText mCreateName, mCreateEmail, mCreatePassword, mCreateCarID;
     private Button mCreateAccount;
     private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,35 +38,41 @@ public class CreateAccountActivity extends AppCompatActivity {
         mCreateAccount = findViewById(R.id.createAccoutButton);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
 
         final String name = mCreateName.getText().toString();
         final String email = mCreateEmail.getText().toString();
         final String password = mCreatePassword.getText().toString();
         final String carId = mCreateCarID.getText().toString();
 
-
-
-//                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d("TAG", "createUserWithEmail:success");
-//                            writeNewDriver(email, password);
-//                            Toast.makeText(LoginActivity.this, "Connection Successful", Toast.LENGTH_SHORT).show();
-//
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
-//                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
-//                        }
-//                    }
-//                });
+        mCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    newUser(email, password, carId);
+                                    Toast.makeText(CreateAccountActivity.this, "User was created", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(CreateAccountActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 
-    private void writeNewDriver(String name, String email, String password, String carId) {
-        Driver driver = new Driver(name, email, password, carId);
-        mDatabase.child("Users").child("Drivers").setValue(driver);
+    private void newUser(String email, String password, String driverId) {
+        Driver driver = new Driver(email, password);
+        mDatabase.child("Users").child("Drivers").child(driverId).setValue(driver);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
